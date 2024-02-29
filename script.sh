4e0e5ac2edc7482db7c420ca8abb8fdb8f4c4f45
Andres Amaya <leon.amaya270@gmail.com>
2024-02-29 22:11:19 +0000

    Fix

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+chmod +x script.sh
 
 echo "Esto sale"
 apt-get install git -y
 apt-get install nodejs -y
+apt-get install npm -y
 git clone https://github.com/Hacktiago/Backend.git --progress
 cd Backend/
 npm install  && echo "------Dependencias instaladas------"
 node server.js
+
