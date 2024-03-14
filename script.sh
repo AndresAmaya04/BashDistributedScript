1de3bb48de894748ae97d3f4bebc1a16bac1e8f2
AndresAmaya04 <67525476+AndresAmaya04@users.noreply.github.com>
2024-03-14 16:02:22 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,5 @@ apt-get install git -y
 apt-get install nodejs -y
 apt-get install npm -y
 git clone https://github.com/AndresAmaya04/FrontLab01.git --progress
-cd Backend/
 npm install  && echo "------Dependencias instaladas------"
-node server.js
 
