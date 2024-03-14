4e9e9c8db6c9f74d1c48fecc325329935765d0c4
AndresAmaya04 <67525476+AndresAmaya04@users.noreply.github.com>
2024-03-14 15:54:23 -0500

    uodateeeeeee

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo "Esto sale"
 apt-get install git -y
 apt-get install nodejs -y
 apt-get install npm -y
-git clone https://github.com/Hacktiago/Backend.git --progress
+git clone https://github.com/AndresAmaya04/FrontLab01.git --progress
 cd Backend/
 npm install  && echo "------Dependencias instaladas------"
 node server.js
